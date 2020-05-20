75487339fb09ef4157eeb53814877d4c95d653fe
abhiJit1993 <abhijitsom1993@gmail.com>
2020-05-20 19:47:26 +0530

    three ways for conditional rendering in JSX:

--- index.js
+++ index.js
@@ -3,10 +3,21 @@
  import React, { Component } from 'react';
   import ReactDom , { Component } from 'react-dom';
 
-// 
-// import React, { Component } from 'react';
-import { render } from 'react-dom';
+const getFirstname = (user_details) => user_details.name.split(' ')[0];
 
-console.log('Hey')
-var template =  <h1> Hey there </h1>;
+var user = {
+  name : 'Abhijit Som',
+  age : 20,
+  city : 'Ranchi'
+}
+
+console.log('Hey');
+
+var template =  
+(<div>
+<h1> Hey {getFirstname(user)} </h1> 
+<p>your current age is : {user.age>=18 ? user.age : 'Not 18'}</p>
+
+{(user && user.city) && (<h2>you are from : {user.city}</h2>)}
+</div>);
 ReactDom.render(template,document.getElementById('app'))
