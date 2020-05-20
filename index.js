4b162b0748adb6f58751506eb043b271c71d440a
abhiJit1993 <abhijitsom1993@gmail.com>
2020-05-20 20:34:28 +0530

    arrow function usecase with map function

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 
- console.log('JRJKVJKKSKKGFG')
  import React, { Component } from 'react';
-  import ReactDom , { Component } from 'react-dom';
+ import ReactDom , { Component } from 'react-dom';
 
 const getFirstname = (user_details) => user_details.name.split(' ')[0];
 
-var user = {
+let user = {
   name : 'Abhijit Som',
   age : 20,
-  city : 'Ranchi'
+  city : 'Ranchi city'
 }
+let resultArray;
+     var multiplier = {
+numbers : [3,9,7,8],
+multiplyBy : 4,
+multiply () {
+ return this.numbers.map((num) => this.multiplyBy * num );
+}
+}
+console.log(multiplier.multiply())
 
-console.log('Hey');
 
-var template =  
-(<div>
+const template =  
+
+(
+<div>
 <h1> Hey {getFirstname(user)} </h1> 
 <p>your current age is : {user.age>=18 ? user.age : 'Not 18'}</p>
 
-{(user && user.city) && (<h2>you are from : {user.city}</h2>)}
-</div>);
+{(user && user.city) && (<h2>You are from : {user.city}</h2>)}
+</div>
+);
+
+
 ReactDom.render(template,document.getElementById('app'))
