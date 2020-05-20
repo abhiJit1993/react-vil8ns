9cf45c006fde72e47509e1bee1693ee94f664be9
abhiJit1993 <abhijitsom1993@gmail.com>
2020-05-20 21:15:49 +0530

    upto this point we can see that although in console, the count value changed but the updated value of count is not rendered to the screen,. The possible reason for this behaviour is  "JSX does not have aautomatic data binding". We will fix this in next commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  import React, { Component } from 'react';
  import ReactDom , { Component } from 'react-dom';
 
+
+
+// 1. CONDITIONAL RENDERING IN JSX
 const getFirstname = (user_details) => user_details.name.split(' ')[0];
 
 let user = {
@@ -9,17 +12,6 @@ let user = {
   age : 20,
   city : 'Ranchi city'
 }
-let resultArray;
-     var multiplier = {
-numbers : [3,9,7,8],
-multiplyBy : 4,
-multiply () {
- return this.numbers.map((num) => this.multiplyBy * num );
-}
-}
-console.log(multiplier.multiply())
-
-
 const template =  
 
 (
@@ -31,5 +23,51 @@ const template =
 </div>
 );
 
+// 2. SOME ES6 FEATURE -  ARROW FUNCTION AND MAP FUNCTION
+let resultArray;
+     var multiplier = {
+numbers : [3,9,7,8],
+multiplyBy : 4,
+multiply () {
+ return this.numbers.map((num) => this.multiplyBy * num );
+}
+}
+console.log(multiplier.multiply());
 
-ReactDom.render(template,document.getElementById('app'))
+//3. EVENTS AND ATTRIBUTES;
+let count = 0 ;
+const addOne = () => {
+count++;
+  console.log("one added....",count);
+  
+   }
+   const subtractOne = () => {
+     count--
+  console.log("one subtacted....",count);
+  
+   }
+   const reset = () => {
+     count = 0;
+  console.log("value reset.....",count)
+   }
+
+let counter = (
+  <div>
+  <h1> Count : {count} </h1>
+  <button onClick={addOne}> +1</button>   &nbsp; &nbsp;&nbsp;&nbsp;
+  <button
+   onClick={subtractOne}> -1</button>   
+   &nbsp; &nbsp;&nbsp;&nbsp;
+  <button
+   onClick={reset}>Reset</button>   
+  </div>
+)
+
+
+
+
+//1.
+//ReactDom.render(template,document.getElementById('app'));
+
+//2.
+ReactDom.render(counter,document.getElementById('app'))
